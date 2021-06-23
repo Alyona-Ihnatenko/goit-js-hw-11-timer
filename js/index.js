f8ac3b126c85c5ff02980ac62577ba10cea6ab1e
Alyona Ihnatenko <altaig4@gmail.com>
2021-06-23 21:19:46 +0300

    end

--- js/index.js
+++ js/index.js
@@ -1,50 +1,56 @@
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jun 30, 2021'),
-//   });
-  
-function pad(value){
-    return String(value).padStart(2,'0');
+const daysEl = document.querySelector('[data-value="days"]');
+const hoursEl = document.querySelector('[data-value="hours"]');
+const minsEl = document.querySelector('[data-value="mins"]');
+const secsEl = document.querySelector('[data-value="secs"]');
+
+class CountdownTimer {
+  constructor({ selector, targetDate, onTick }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.onTick = onTick;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+  getComponentsTime(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  start() {
+    this.id = setInterval(() => {
+      const dateNow = Date.now();
+      const diff = this.targetDate - dateNow;
+      const { days, hours, mins, secs } = this.getComponentsTime(diff);
+      setTimer({ days, hours, mins, secs });
+      this.stop(diff);
+    }, 1000);
+  }
+
+  stop(time) {
+    if (time <= 0) {
+      clearInterval(this.id);
+    }
+  }
 }
 
-function getComponentsTime(time){
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = pad( Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins =  pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad( Math.floor((time % (1000 * 60)) / 1000));
-    return {days,hours,mins,secs};
-}
+const myBirthdayDay= new  CountdownTimer({
+onTick: setTimer,
+selector: '#timer-1',
+targetDate: new Date('jun 30, 2021'),
 
+});
+myBirthdayDay.start();
 
-const myBirthdayDate = new Date('Jun 30, 2021').getTime();
-
-function Timer({selector}) {
-    let id = 0;
-    document.querySelector(selector)
-        .insertAdjacentHTML('beforeend', template('0:0:0:0'));
-
-    const valueRef = document.querySelector(`${selector} .value`);
-
-    const updateValue = (value) => {
-        valueRef.textContent = value;
-    }
-
-    this.start = () => {
-        if(id) {
-            return;
-        }
-        id = setInterval(() => {
-            const diff = myBirthdayDate - Date.now();
-            const days = days(diff);
-            const hours = hours(diff);
-            const mins = mins(diff);
-            const seconds = secs(diff);
-            updateValue(`${days} days ${hours} hours ${mins} mins ${seconds} seconds`);
-        }, 100);
-    };
-
-    this.stop = () => {
-        clearTimeout(id);
-        id = 0;
-    }
+function setTimer({ days, hours, mins, secs }) {
+  daysEl.textContent = `${days}`;
+  hoursEl.textContent = `${hours}`;
+  minsEl.textContent = `${mins}`;
+  secsEl.textContent = `${secs}`;
 }
